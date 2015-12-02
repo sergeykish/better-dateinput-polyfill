100f74591b42ea2675b0ddeff8748d5f873fb686
Sergey Kish <s.kishinets@svitla.com>
2015-12-02 13:07:39 +0200

    move listeners out

--- src/main.js
+++ src/main.js
@@ -26,11 +26,11 @@
                 // IE8 doesn't suport color:transparent - use background-color instead
                 .css("color", document.addEventListener ? "transparent" : this.css("background-color"))
                 // handle arrow keys, esc etc.
-                .on("keydown", ["which", "shiftKey"], this._keydownCalendar.bind(this, calendar))
+                .on("keydown", ["which", "shiftKey"], _keydownCalendar.bind(this, calendar))
                 // sync picker visibility on focus/blur
-                .on(["focus", "click"], this._focusCalendar.bind(this, calendar))
-                .on("blur", this._blurCalendar.bind(this, calendar))
-                .on("change", this._formatValue.bind(this, label))
+                .on(["focus", "click"], _focusCalendar.bind(this, calendar))
+                .on("blur", _blurCalendar.bind(this, calendar))
+                .on("change", _formatValue.bind(this, label))
                 .before(calendar.hide(), label);
 
             label
@@ -40,14 +40,14 @@
 
             var calenderDays = calendar.findAll(`.${BASE_CLASS}-calendar-body`),
                 calendarCaption = calendar.find(`.${BASE_CLASS}-calendar-caption`),
-                changeValue = this._changeValue.bind(this, calendarCaption, calenderDays, calendar);
+                changeValue = _changeValue.bind(this, calendarCaption, calenderDays, calendar);
 
-            this.closest("form").on("reset", this._resetForm.bind(this));
+            this.closest("form").on("reset", _resetForm.bind(this));
             this.watch("value", changeValue);
             // trigger watchers to build the calendar
             changeValue(this.value());
 
-            calendar.on("mousedown", ["target"], this._clickCalendar.bind(this, calendar));
+            calendar.on("mousedown", ["target"], _clickCalendar.bind(this, calendar));
             window.requestAnimationFrame(() => {
                 var offset = this.offset();
                 var labelOffset = label.offset();
@@ -75,190 +75,190 @@
                 // display calendar for autofocused elements
                 if (this.matches(":focus")) this.fire("focus");
             });
-        },
-        _changeValue(caption, calenderDays, calendar, value, prevValue) {
-            var year, month, date, iterDate;
+        }
+    })
+    function _changeValue(caption, calenderDays, calendar, value, prevValue) {
+        var year, month, date, iterDate;
 
-            value = new Date(value);
+        value = new Date(value);
 
-            if (!value.getTime()) {
-                value = new Date();
+        if (!value.getTime()) {
+            value = new Date();
+        }
+
+        month = value.getUTCMonth();
+        date = value.getUTCDate();
+        year = value.getUTCFullYear();
+        // update calendar caption
+        caption.set(`<span><span data-l10n="_">${DateUtils.MONTHS[month]}</span></span> ${year}`);
+        // update calendar content
+        iterDate = new Date(Date.UTC(year, month, 0));
+        // move to beginning of current month week
+        iterDate.setUTCDate(iterDate.getUTCDate() - iterDate.getUTCDay() - ampm(1, 0));
+
+        prevValue = new Date(prevValue);
+
+        var delta = value.getUTCMonth() - prevValue.getUTCMonth() + 100 * (value.getUTCFullYear() - prevValue.getUTCFullYear());
+        var currenDays = calenderDays[calendar.contains(calenderDays[0]) ? 0 : 1];
+        var targetDays = delta ? calenderDays[calenderDays[0] === currenDays ? 1 : 0] : currenDays;
+        var range = readDateRange(this);
+
+        // update days
+        targetDays.findAll("td").forEach((day) => {
+            iterDate.setUTCDate(iterDate.getUTCDate() + 1);
+
+            var mDiff = month - iterDate.getUTCMonth(),
+                className = `${BASE_CLASS}-calendar-`;
+
+            if (year !== iterDate.getUTCFullYear()) mDiff *= -1;
+
+            if (iterDate < range[0] || iterDate > range[1]) {
+                className += "out";
+            } else if (mDiff > 0) {
+                className += "past";
+            } else if (mDiff < 0) {
+                className += "future";
+            } else if (date === iterDate.getUTCDate()) {
+                className += "today";
+            } else {
+                className = "";
             }
 
-            month = value.getUTCMonth();
-            date = value.getUTCDate();
-            year = value.getUTCFullYear();
-            // update calendar caption
-            caption.set(`<span><span data-l10n="_">${DateUtils.MONTHS[month]}</span></span> ${year}`);
-            // update calendar content
-            iterDate = new Date(Date.UTC(year, month, 0));
-            // move to beginning of current month week
-            iterDate.setUTCDate(iterDate.getUTCDate() - iterDate.getUTCDay() - ampm(1, 0));
+            day.set({
+                _ts: iterDate.getTime(),
+                className: className,
+                textContent: iterDate.getUTCDate()
+            });
+        });
 
-            prevValue = new Date(prevValue);
+        if (delta) {
+            currenDays[delta > 0 ? "after" : "before"](targetDays);
+            currenDays.hide(() => { currenDays.remove() });
+            targetDays.show();
+        }
 
-            var delta = value.getUTCMonth() - prevValue.getUTCMonth() + 100 * (value.getUTCFullYear() - prevValue.getUTCFullYear());
-            var currenDays = calenderDays[calendar.contains(calenderDays[0]) ? 0 : 1];
-            var targetDays = delta ? calenderDays[calenderDays[0] === currenDays ? 1 : 0] : currenDays;
+        // trigger event manually to notify about changes
+        this.fire("change");
+    }
+    function _formatValue(label) {
+        var value = new Date(this.get()),
+            formattedValue = "";
+
+        if (value.getTime()) {
+            var date = value.getUTCDate();
+            var month = value.getUTCMonth();
+            var year = value.getUTCFullYear();
+            formattedValue = `${pad(month + 1, 2)}/${pad(date, 2)}/${year}`
+        }
+
+        // display formatted date value instead of real one
+        label.value(formattedValue);
+    }
+    function _clickCalendar(calendar, target) {
+        var targetDate;
+
+        if (target.matches("a")) {
+            targetDate = new Date(this.get());
+
+            if (!targetDate.getTime()) targetDate = new Date();
+
+            targetDate.setUTCMonth(targetDate.getUTCMonth() + (target.next("a")[0] ? -1 : 1));
+        } else if (target.matches("td")) {
+            targetDate = target.get("_ts");
+
+            if (targetDate) {
+                targetDate = new Date(targetDate);
+                calendar.hide();
+            }
+        }
+
+        if (targetDate != null) {
             var range = readDateRange(this);
 
-            // update days
-            targetDays.findAll("td").forEach((day) => {
-                iterDate.setUTCDate(iterDate.getUTCDate() + 1);
+            if (targetDate < range[0]) {
+                targetDate = range[0];
+            } else if (targetDate > range[1]) {
+                targetDate = range[1];
+            }
 
-                var mDiff = month - iterDate.getUTCMonth(),
-                    className = `${BASE_CLASS}-calendar-`;
+            this.value(formatISODate(targetDate));
+        }
+        // prevent input from loosing focus
+        return false;
+    }
+    function _keydownCalendar(calendar, which, shiftKey) {
+        var delta, currentDate;
 
-                if (year !== iterDate.getUTCFullYear()) mDiff *= -1;
+        // ENTER key should submit form if calendar is hidden
+        if (calendar.matches(":hidden") && which === VK_ENTER) return true;
 
-                if (iterDate < range[0] || iterDate > range[1]) {
-                    className += "out";
-                } else if (mDiff > 0) {
-                    className += "past";
-                } else if (mDiff < 0) {
-                    className += "future";
-                } else if (date === iterDate.getUTCDate()) {
-                    className += "today";
-                } else {
-                    className = "";
-                }
+        if (which === VK_SPACE) {
+            calendar.toggle(); // SPACE key toggles calendar visibility
+        } else if (which === VK_ESCAPE || which === VK_TAB || which === VK_ENTER) {
+            calendar.hide(); // ESC, TAB or ENTER keys hide calendar
+        } else if (which === VK_BACKSPACE || which === VK_DELETE) {
+            this.empty(); // BACKSPACE, DELETE clear value
+        } else {
+            currentDate = new Date(this.get());
 
-                day.set({
-                    _ts: iterDate.getTime(),
-                    className: className,
-                    textContent: iterDate.getUTCDate()
-                });
-            });
+            if (!currentDate.getTime()) currentDate = new Date();
+
+            if (which === 74 || which === 40) { delta = 7; }
+            else if (which === 75 || which === 38) { delta = -7; }
+            else if (which === 76 || which === 39) { delta = 1; }
+            else if (which === 72 || which === 37) { delta = -1; }
 
             if (delta) {
-                currenDays[delta > 0 ? "after" : "before"](targetDays);
-                currenDays.hide(() => { currenDays.remove() });
-                targetDays.show();
-            }
-
-            // trigger event manually to notify about changes
-            this.fire("change");
-        },
-        _formatValue(label) {
-            var value = new Date(this.get()),
-                formattedValue = "";
-
-            if (value.getTime()) {
-                var date = value.getUTCDate();
-                var month = value.getUTCMonth();
-                var year = value.getUTCFullYear();
-                formattedValue = `${pad(month + 1, 2)}/${pad(date, 2)}/${year}`
-            }
-
-            // display formatted date value instead of real one
-            label.value(formattedValue);
-        },
-        _clickCalendar(calendar, target) {
-            var targetDate;
-
-            if (target.matches("a")) {
-                targetDate = new Date(this.get());
-
-                if (!targetDate.getTime()) targetDate = new Date();
-
-                targetDate.setUTCMonth(targetDate.getUTCMonth() + (target.next("a")[0] ? -1 : 1));
-            } else if (target.matches("td")) {
-                targetDate = target.get("_ts");
-
-                if (targetDate) {
-                    targetDate = new Date(targetDate);
-                    calendar.hide();
+                if (shiftKey && (which === 40 || which === 38)) {
+                    currentDate.setUTCFullYear(currentDate.getUTCFullYear() + (delta > 0 ? 1 : -1));
+                } else if (shiftKey && (which === 37 || which === 39)) {
+                    currentDate.setUTCMonth(currentDate.getUTCMonth() + (delta > 0 ? 1 : -1));
+                } else {
+                    currentDate.setUTCDate(currentDate.getUTCDate() + delta);
                 }
-            }
 
-            if (targetDate != null) {
                 var range = readDateRange(this);
 
-                if (targetDate < range[0]) {
-                    targetDate = range[0];
-                } else if (targetDate > range[1]) {
-                    targetDate = range[1];
-                }
-
-                this.value(formatISODate(targetDate));
-            }
-            // prevent input from loosing focus
-            return false;
-        },
-        _keydownCalendar(calendar, which, shiftKey) {
-            var delta, currentDate;
-
-            // ENTER key should submit form if calendar is hidden
-            if (calendar.matches(":hidden") && which === VK_ENTER) return true;
-
-            if (which === VK_SPACE) {
-                calendar.toggle(); // SPACE key toggles calendar visibility
-            } else if (which === VK_ESCAPE || which === VK_TAB || which === VK_ENTER) {
-                calendar.hide(); // ESC, TAB or ENTER keys hide calendar
-            } else if (which === VK_BACKSPACE || which === VK_DELETE) {
-                this.empty(); // BACKSPACE, DELETE clear value
-            } else {
-                currentDate = new Date(this.get());
-
-                if (!currentDate.getTime()) currentDate = new Date();
-
-                if (which === 74 || which === 40) { delta = 7; }
-                else if (which === 75 || which === 38) { delta = -7; }
-                else if (which === 76 || which === 39) { delta = 1; }
-                else if (which === 72 || which === 37) { delta = -1; }
-
-                if (delta) {
-                    if (shiftKey && (which === 40 || which === 38)) {
-                        currentDate.setUTCFullYear(currentDate.getUTCFullYear() + (delta > 0 ? 1 : -1));
-                    } else if (shiftKey && (which === 37 || which === 39)) {
-                        currentDate.setUTCMonth(currentDate.getUTCMonth() + (delta > 0 ? 1 : -1));
-                    } else {
-                        currentDate.setUTCDate(currentDate.getUTCDate() + delta);
-                    }
-
-                    var range = readDateRange(this);
-
-                    if (!(currentDate < range[0] || currentDate > range[1])) {
-                        this.value(formatISODate(currentDate));
-                    }
+                if (!(currentDate < range[0] || currentDate > range[1])) {
+                    this.value(formatISODate(currentDate));
                 }
             }
-            // prevent default action except if it was TAB so
-            // do not allow to change the value manually
-            return which === VK_TAB;
-        },
-        _blurCalendar(calendar) {
-            calendar.hide();
-        },
-        _focusCalendar(calendar) {
-            // update calendar weekday captions
-            calendar.findAll("th").forEach((el, index) => {
-                el.value(`<span data-l10n="_">${DateUtils.DAYS[ampm(index, ++index % 7)].slice(0, 2)}</span>`)
-            });
-
-            calendar.show();
-
-            // use the trick below to reset text selection on focus
-            setTimeout(() => {
-                var node = this[0];
-
-                if ("selectionStart" in node) {
-                    node.selectionStart = 0;
-                    node.selectionEnd = 0;
-                } else {
-                    var inputRange = node.createTextRange();
-
-                    inputRange.moveStart("character", 0);
-                    inputRange.collapse();
-                    inputRange.moveEnd("character", 0);
-                    inputRange.select();
-                }
-            }, 0);
-        },
-        _resetForm() {
-            this.value(this.get("defaultValue"));
         }
-    });
+        // prevent default action except if it was TAB so
+        // do not allow to change the value manually
+        return which === VK_TAB;
+    }
+    function _blurCalendar(calendar) {
+        calendar.hide();
+    }
+    function _focusCalendar(calendar) {
+        // update calendar weekday captions
+        calendar.findAll("th").forEach((el, index) => {
+            el.value(`<span data-l10n="_">${DateUtils.DAYS[ampm(index, ++index % 7)].slice(0, 2)}</span>`)
+        });
+
+        calendar.show();
+
+        // use the trick below to reset text selection on focus
+        setTimeout(() => {
+            var node = this[0];
+
+            if ("selectionStart" in node) {
+                node.selectionStart = 0;
+                node.selectionEnd = 0;
+            } else {
+                var inputRange = node.createTextRange();
+
+                inputRange.moveStart("character", 0);
+                inputRange.collapse();
+                inputRange.moveEnd("character", 0);
+                inputRange.select();
+            }
+        }, 0);
+    }
+    function _resetForm() {
+        this.value(this.get("defaultValue"));
+    }
 }(window.DOM, "btr-dateinput", 32, 9, 13, 27, 8, 46, {
     DAYS: "Sunday Monday Tuesday Wednesday Thursday Friday Saturday".split(" "),
     MONTHS: "January February March April May June July August September October November December".split(" "),
